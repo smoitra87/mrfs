5124a046a5d013feeaebae92a23c3bd47209d4b6
Subhodeep Moitra <smoitra@cs.cmu.edu>
2015-01-06 15:31:51 -0500

    Calculate imputation error and pll

---
+++ evaluate_HMRF_Pll.m
@@ -0,0 +1,99 @@
+function [pll, impErr] = evaluate_HMRF_Pll(paramf, msaf, outf)
+
+load(paramf, 'adj', 'edgeMap', 'edgePot', 'edgeStruct', 'infoStruct', ...
+    'nodeMap', 'nodePot', 'w');
+
+%% Load the test data file
+
+[names,y] = textread(msaf, '%s %s');
+if(isempty(y{1}))
+  y = textread(msaf, '%s');
+end;
+y = converttonumericmsa(y);
+y = y-min(min(y));
+[nInstances,nVisNodes] = size(y);
+
+if isfield(infoStruct, 'hasHidden')
+   hasHidden = infoStruct.hasHidden;
+else
+   hasHidden = 0;
+end
+
+if hasHidden
+    nHidNodes = nVisNodes;
+else
+    nHidNodes = 0;
+end
+
+nNodes = nVisNodes + nHidNodes;
+y = y+1;
+y = [y zeros(nInstances, nHidNodes)];
+y = int32(y);
+
+%% Load inference related data
+
+if isfield(infoStruct, 'inferFunc')
+   inferFunc = parseInferFunc(infoStruct.inferFunc);
+else
+   inferFunc = @UGM_Infer_LBP;
+end
+
+
+if isfield(infoStruct, 'condInferFunc')
+   condInferFunc = parseInferFunc(infoStruct.condInferFunc);
+else
+   condInferFunc = @UGM_Infer_LBP;
+end
+
+
+%% Calculate Log likelihood
+
+
+[nodeBel,edgeBel,logZ] = inferFunc(nodePot,edgePot,edgeStruct);
+
+%% Do conditional inference
+err = 0;
+pll = zeros(nInstances, 1);
+for i = 1:nInstances
+    for j = i:nVisNodes
+        clamped = y(i,:); clamped(j) = 0;
+        [nodeBelC,edgeBelC,logZC] = UGM_Infer_Conditional(nodePot,edgePot,...
+            edgeStruct,clamped,condInferFunc);
+        [maxMarg,maxMargIdx] = max(nodeBelC(j,:));
+        err = err + (maxMargIdx ~= y(i,j));
+        pll(i) = pll(i) + log(nodeBelC(j,y(i,j)));
+    end
+end
+impErr = err/(nInstances*nVisNodes);
+pll = mean(pll);
+
+
+fprintf('Average PLL : %f\n', pll);
+fprintf('Imputation Error : %f\n', impErr);
+end
+
+
+function [logZC_update] = update_logZC(Y, i, nodePot, edgePot, edgeEnds)
+
+y = Y(i,:);
+logZC_update = 0;
+nNodes = size(nodePot,1);
+nEdges = size(edgePot,3);
+
+for n = 1:nNodes
+    if y(n)~=0
+        logZC_update = logZC_update + log(nodePot(n,y(n)));
+    end
+end
+for e = 1:nEdges
+    n1 = edgeEnds(e,1);
+    n2 = edgeEnds(e,2);
+    if y(n1)~=0 && y(n2)~=0
+        logZC_update = logZC_update + log(edgePot(y(n1),y(n2),e));
+    end
+end
+
+end
+
+
+
