cc61fafb0f5c8708423c838381e7d3ae9a482e87
Subhodeep Moitra <smoitra@cs.cmu.edu>
2015-04-23 11:31:37 -0400

    Gets latest results

--- results/create_csv.py
+++ results/create_csv.py
@@ -18,6 +18,10 @@ if __name__ == '__main__':
     blosumfs = [f for f in files if '_blosum' in f ]
     llfs = [f for f in files if '_ll' in f ]
     paramfs = [f for f in files if '_params' in f ]
+    randfs_dict, randfsbl90_dict = {}, {}
+    for ncol in (2,3,5,10):
+        randfs_dict[ncol] = [f for f in files if '_rand{}multimp'.format(ncol) in f]
+        randfsbl90_dict[ncol] = [f for f in files if '_rand{}multbl90'.format(ncol) in f]
 
     mat = sio.loadmat(paramfs[0], squeeze_me=True)
     infoStruct = mat['infoStruct']
@@ -25,13 +29,19 @@ if __name__ == '__main__':
 
     remove_headers = ['hasHidden', 'seed', 'options', 'adj', 'useMex']
     headers = sorted([h for h in headers if h not in remove_headers])
-    headers = ['jobkey', 'train-ll','valid-ll', 'test-ll'] + \
+    headers2 = ['jobkey', 'train-ll','valid-ll', 'test-ll'] + \
             ['train-pll', 'valid-pll', 'test-pll'] + \
             ['train-imperr', 'valid-imperr', 'test-imperr'] + \
             ['train-bl90', 'valid-bl90', 'test-bl90'] + \
             ['train-imperr-serr', 'valid-imperr-serr', 'test-imperr-serr'] +\
-            ['train-bl90-serr', 'valid-bl90-serr', 'test-bl90-serr'] +\
-            headers;
+            ['train-bl90-serr', 'valid-bl90-serr', 'test-bl90-serr'];
+    for ncol in (2,3,5,10):
+        for t in ('train', 'valid','test'):
+            headers2.append('{}-rand{}'.format(t,ncol))
+    for ncol in (2,3,5,10):
+        for t in ('train', 'valid','test'):
+            headers2.append('{}-rand{}bl90'.format(t,ncol))
+    headers = headers2 + headers;
 
     records = []
     for paramf in paramfs:
@@ -42,6 +52,10 @@ if __name__ == '__main__':
         pllf = [f for f in pllfs if base_key in f]
         blosumf = [f for f in blosumfs if base_key in f]
 
+        randf_dict, randfbl90_dict = {}, {}
+        for ncol in (2,3,5,10):
+            randf_dict[ncol] = [f for f in randfs_dict[ncol] if base_key in f]
+            randfbl90_dict[ncol] = [f for f in randfsbl90_dict[ncol] if base_key in f]
         mat = sio.loadmat(paramf, squeeze_me=True)
         infoStruct = mat['infoStruct']
 
@@ -57,6 +71,20 @@ if __name__ == '__main__':
             metric['{}-imperr-serr'.format(t)] = None
             metric['{}-bl90-serr'.format(t)] = None
 
+        for ncol in (2,3,5,10):
+            for t in ('train', 'valid','test'):
+                metric['{}-rand{}'.format(t,ncol)] = None
+                metric['{}-rand{}bl90'.format(t,ncol)] = None
+
+        for ncol in (2,3,5,10):
+            for f in randf_dict[ncol] :
+                for t in ('train', 'valid','test'):
+                    if t in f:
+                        metric['{}-rand{}'.format(t,ncol)] = sio.loadmat(f, squeeze_me=True)['impErr']
+            for f in randfbl90_dict[ncol] :
+                for t in ('train', 'valid','test'):
+                    if t in f:
+                        metric['{}-rand{}bl90'.format(t,ncol)] = sio.loadmat(f, squeeze_me=True)['impErr']
 
         for f in llf :
             for t in ('train', 'valid', 'test'):
